10b6c34c6da4198dd2f5ad88a658a0f0d7bc431d
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-06 01:53:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,11 @@
 def speak_to_grandma(phrase)
-  phrase = "Hey, Grandma"
-if phrase = "How are you Grandma?"
-  puts phrase = "HUH?! SPEAK UP, SONNY!"
-end
-if phrase = "I LOVE YOU GRANDMA"
-  puts phrase = "I LOVE YOU TOO PUMPKIN"
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
